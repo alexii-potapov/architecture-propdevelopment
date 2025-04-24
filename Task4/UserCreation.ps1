939f3c53b43c71b4eb1e14893668fbe50892bf05
Потапов Алексей <potapov@anc.space>
2025-04-24 16:23:27 +0300

    Clean

--- Task4/UserCreation.ps1
+++
@@ -1,3 +0,0 @@
-kubectl config set-credentials ivanov  --username=ivanov  --password=pass1
-kubectl config set-credentials petrov  --username=petrov  --password=pass2
-kubectl config set-credentials sidorov  --username=sidorov  --password=pass3
